c8a73473423b55c42dbc249f1914e0533d910850
Mick <mickx.shular@gmail.com>
2020-11-16 20:52:22 -0500

    Change Start scene

--- SDL_Engine-master/src/StartScene.cpp
+++ SDL_Engine-master/src/StartScene.cpp
@@ -46,39 +46,59 @@ void StartScene::handleEvents()
 void StartScene::start()
 {
 	const SDL_Color blue = { 0, 0, 255, 255 };
-	m_pStartLabel = new Label("START SCENE", "Consolas", 80, blue, glm::vec2(400.0f, 40.0f));
+	m_pStartLabel = new Label("Team members", "Consolas", 80, blue, glm::vec2(400.0f, 40.0f));
 	m_pStartLabel->setParent(this);
 	addChild(m_pStartLabel);
 
-	m_pInstructionsLabel = new Label("Press 1 to Play", "Consolas", 40, blue, glm::vec2(400.0f, 120.0f));
+	m_pInstructionsLabel = new Label("Daekoen Lee - 101076401", "Consolas", 40, blue, glm::vec2(400.0f, 120.0f));
 	m_pInstructionsLabel->setParent(this);
 	addChild(m_pInstructionsLabel);
 
+	m_pInstructionsLabel = new Label("Michael Shular - 101273089", "Consolas", 40, blue, glm::vec2(400.0f, 160.0f));
+	m_pInstructionsLabel->setParent(this);
+	addChild(m_pInstructionsLabel);
 
-	m_pShip = new Ship();
-	m_pShip->getTransform()->position = glm::vec2(400.0f, 300.0f); 
-	addChild(m_pShip); 
+	// Scene 2 Button
+	m_pScene1Button = new Button();
+	m_pScene1Button->getTransform()->position = glm::vec2(400.0f, 400.0f);
 
-	// Start Button
-	m_pStartButton = new Button();
-	m_pStartButton->getTransform()->position = glm::vec2(400.0f, 400.0f); 
-
-	m_pStartButton->addEventListener(CLICK, [&]()-> void
+	m_pScene1Button->addEventListener(CLICK, [&]()-> void
 	{
-		m_pStartButton->setActive(false);
+			m_pScene1Button->setActive(false);
 		TheGame::Instance()->changeSceneState(PLAY_SCENE);
 	});
 	
-	m_pStartButton->addEventListener(MOUSE_OVER, [&]()->void
+	m_pScene1Button->addEventListener(MOUSE_OVER, [&]()->void
 	{
-		m_pStartButton->setAlpha(128);
+			m_pScene1Button->setAlpha(128);
 	});
 
-	m_pStartButton->addEventListener(MOUSE_OUT, [&]()->void
+	m_pScene1Button->addEventListener(MOUSE_OUT, [&]()->void
 	{
-		m_pStartButton->setAlpha(255);
+			m_pScene1Button->setAlpha(255);
 	});
-	addChild(m_pStartButton);
+	addChild(m_pScene1Button);
+
+	// Scene 2 Button
+	m_pScene2Button = new Button("../Assets/textures/nextButton.png", "nextButton", NEXT_BUTTON);
+	m_pScene2Button->getTransform()->position = glm::vec2(400.0f, 500.0f);
+
+	m_pScene2Button->addEventListener(CLICK, [&]()-> void
+		{
+			m_pScene2Button->setActive(false);
+			TheGame::Instance()->changeSceneState(END_SCENE);
+		});
+
+	m_pScene2Button->addEventListener(MOUSE_OVER, [&]()->void
+		{
+			m_pScene2Button->setAlpha(128);
+		});
+
+	m_pScene2Button->addEventListener(MOUSE_OUT, [&]()->void
+		{
+			m_pScene2Button->setAlpha(255);
+		});
+	addChild(m_pScene2Button);
 
 	
 }
